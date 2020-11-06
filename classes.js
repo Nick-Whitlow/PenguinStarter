4185824ad61ce48fe1c3990c3ff25c920558cf2b
Nick Whitlow <nickwhitlow@Gracies-MacBook-Air.local>
2020-11-06 10:28:24 -0500

    added sort

--- classes.js
+++ classes.js
@@ -33,20 +33,15 @@ var finalMean = function(penguin){
     })
 return d3.mean(studentFinal).toFixed(2)
 }
-
+ 
 
 var clearTable = function() {
     d3.selectAll("tbody tr")
     .remove();
 }
 
-//var initHeaders = function(penguins){
-    
-
-var successFCN = function(penguins){
-    console.log(penguins);
-    
-    var rows = d3.select("tbody")
+var drawTable = function(penguins){
+    var rows = d3.select(" tbody")
     .selectAll("tr")
     .data(penguins)
     .enter()
@@ -54,23 +49,55 @@ var successFCN = function(penguins){
     
     rows.append("td")
     .append("img")
+    .attr("class", "pict")
     .attr("src", getIMG);
     
     rows.append("td")
+    .attr("class", "quiz")
     .text(quizMeans);
     
     rows.append("td")
+    .attr("class", "hw")
     .text(hwMeans);
     
     rows.append("td")
+    .attr("class", "test")
     .text(testMean);
     
     rows.append("td")
     .attr("class", "finalGrade")
     .text(finalMean);
-    
-    
+};
 
+var clearTable = function() {
+    d3.selectAll("tbody tr")
+    .remove();
+}
+    
+var initHeaders = function(penguins){
+    d3.select("#finalGrade")
+    .on("click", function(){
+        console.log("clicked quiz");
+        penguins.sort(function(a,b) {
+            var av1 = finalMean(a);
+            var av2 = finalMean(b);
+            if (av1 < av2) {return 1;}
+            else if(av1 > av2) {return -1;}
+            else {return 0;}
+        });
+        clearTable();
+        drawTable(penguins)
+        d3.selectAll(".finalGrade") 
+        .attr("class","selected");
+        
+    });
+}
+
+var successFCN = function(penguins){
+    console.log("data", penguins);
+    drawTable(penguins);
+    initHeaders(penguins)
+    
 }
 var failFCN = function(errMessage) {
     console.log("failure", errMessage);
